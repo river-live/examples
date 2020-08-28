fa39c288914c6f3aa1a9b85a897ede7269392536
Mark Hyams <markhyams@gmail.com>
2020-08-27 20:11:49 -0600

    added /api/comment_replies route functionality

--- comments-react/data/data.js
+++ comments-react/data/data.js
@@ -1,13 +1,15 @@
-const { readFileSync, writeFileSync } = require('fs');
-const {v4: uuidv4} = require('uuid');
-const path = require('path');
-const stringify = require('json-beautify');
-const DATA_FILE_PATH = path.join(__dirname, 'comments.json');
+const { readFileSync, writeFileSync } = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const stringify = require("json-beautify");
+const DATA_FILE_PATH = path.join(__dirname, "comments.json");
 
 const data = {
   getCommentsWithOneReply: () => {
     const comments = JSON.parse(readFileSync(DATA_FILE_PATH));
-    return comments.map((c) => Object.assign({}, c, {replies: c.replies.slice(0, 1)}));
+    return comments.map((c) =>
+      Object.assign({}, c, { replies: c.replies.slice(0, 1) })
+    );
   },
 
   getRepliesForComment: (id) => {
@@ -16,22 +18,39 @@ const data = {
   },
 
   saveComment: (commentFields) => {
-    const newComment = Object.assign(
-                        {},
-                        commentFields,
-                        {
-                          id: uuidv4(),
-                          postedAt: +Date.now(),
-                          replies_count: 0,
-                          replies: []
-                        }
-    );
+    const newComment = Object.assign({}, commentFields, {
+      id: uuidv4(),
+      postedAt: +Date.now(),
+      replies_count: 0,
+      replies: [],
+    });
 
     let comments = JSON.parse(readFileSync(DATA_FILE_PATH));
     comments = comments.concat(newComment);
     writeFileSync(DATA_FILE_PATH, stringify(comments, null, 2, 100));
     return newComment;
-  }
-}
+  },
+
+  saveReplyToComment: (comment_id, replyFields) => {
+    const newReply = Object.assign({}, replyFields, {
+      id: uuidv4(),
+      postedAt: +Date.now(),
+      comment_id: comment_id,
+    });
+    let comments = JSON.parse(readFileSync(DATA_FILE_PATH));
+    // find comment findIndex
+    const idx = comments.findIndex((comment) => {
+      return comment.id === comment_id;
+    });
+    // concat new reply
+    comments[idx].replies.push(newReply);
+
+    // update reply count
+    comments[idx].replies_count += 1;
+    writeFileSync(DATA_FILE_PATH, stringify(comments, null, 2, 100));
+
+    return newReply;
+  },
+};
 
 module.exports = data;
